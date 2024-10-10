ac873ffe05baca6a389277c890c4c2799fb5184d
Mohamed Elsayed <86891363+Mohamedzonkol@users.noreply.github.com>
2024-10-10 23:16:31 +0300

    Update update_google_sheet.py

--- .github/workflows/update_google_sheet.py
+++ .github/workflows/update_google_sheet.py
@@ -36,7 +36,7 @@ def custom_assignee_name(assignee):
 
 def update_google_sheet(issue_number, issue_title, assignee, assigned_date, closed_at, issue_state, issue_link):
     SPREADSHEET_ID = "17eMiDmtMaqnpfzDzzB5IQyT0rB5udHprYdDlB-W7Krw"  # Replace with your Google Sheet ID
-    RANGE_NAME = "Web!A:J"  # Modify this based on your column structure
+    RANGE_NAME = "Web!A:K"  # Modify this based on your column structure
     service = get_sheets_service()
 
     # Fetch current data from the sheet
@@ -65,7 +65,7 @@ def update_google_sheet(issue_number, issue_title, assignee, assigned_date, clos
     # Check if the issue already exists in the sheet
     issue_row = None
     for i, row in enumerate(values, start=1):  # Start from 1 to match Google Sheets row index
-        if str(row[1]) == str(issue_number):
+        if str(row[2]) == str(issue_number):
             issue_row = i + 1  # Google Sheets is 1-indexed, so add 1 to match
 
     if issue_row:
