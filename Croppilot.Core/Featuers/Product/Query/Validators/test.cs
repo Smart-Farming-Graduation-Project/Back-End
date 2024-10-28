874d18c1b5843b7ee1a7edd3ffa91cfe1c880736
Mohamedzonol <mo.zonkol@gmail.com>
2024-10-28 20:46:17 +0300

    set cqrs files

---
+++ Croppilot.Core/Featuers/Product/Query/Validators/test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Croppilot.Core.Featuers.Product.Query.Validators
+{
+    internal class test
+    {
+    }
+}
