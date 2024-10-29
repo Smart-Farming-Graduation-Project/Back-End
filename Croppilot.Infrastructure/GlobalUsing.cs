dbf07ab13d500c8838076b6e7b994125be6ef28c
Reda Elsayed <redaelsayied.work@gmail.com>
2024-10-29 15:13:18 +0300

    enhancement (IGenericRepository, GenericRepository) code
    add CancellationToken , handle some code cases

--- Croppilot.Infrastructure/GlobalUsing.cs
+++ Croppilot.Infrastructure/GlobalUsing.cs
@@ -3,3 +3,5 @@ global using Croppilot.Infrastructure.Data;
 global using Croppilot.Infrastructure.Repositories.Interfaces;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Microsoft.EntityFrameworkCore.Storage;
+global using System.Linq.Expressions;
--- Croppilot.Infrastructure/Repositories/Implementation/ProductRepository.cs
+++ Croppilot.Infrastructure/Repositories/Implementation/ProductRepository.cs
@@ -1,35 +1,35 @@
 ﻿using Croppilot.Infrastructure.Generics.Implementation;
 
-namespace Croppilot.Infrastructure.Repositories.Implementation
+namespace Croppilot.Infrastructure.Repositories.Implementation;
+
+public class ProductRepository(AppDbContext context) : GenericRepository<Product>(context), IProductRepository
 {
-    public class ProductRepository(AppDbContext context) : GenericRepository<Product>(context), IProductRepository
+    private readonly DbSet<Product> _product = context.Set<Product>();
+
+    //for ex 
+    public async Task<List<Product>> GetProductsByCategoryAsync(int categoryId)
     {
-        private readonly DbSet<Product> _product = context.Set<Product>();
-        //for ex 
-        public async Task<List<Product>> GetProductsByCategoryAsync(int categoryId)
-        {
-            return await _context.Set<Product>()
-                .Where(p => p.CategoryId == categoryId)
-                .AsNoTracking()
-                .ToListAsync();
-        }
-
-        //public override async Task<List<Product>> GetAll()
-        //{
-        //    return await _context.Products
-        //        .Include(p => p.Category)
-        //        .Include(P => P.Leasings)
-        //        .Include(p => p.ProductImages)
-        //        .ToListAsync();
-        //}
-
-        //public override async Task<Product?> GetById(int id)
-        //{
-        //    return await _context.Products
-        //        .Include(p => p.Category)
-        //        .Include(P => P.Leasings)
-        //        .Include(p => p.ProductImages)
-        //        .SingleOrDefaultAsync(p => p.Id == id);
-        //}
+        return await _context.Set<Product>()
+            .Where(p => p.CategoryId == categoryId)
+            .AsNoTracking()
+            .ToListAsync();
     }
+
+    //public override async Task<List<Product>> GetAll()
+    //{
+    //    return await _context.Products
+    //        .Include(p => p.Category)
+    //        .Include(P => P.Leasings)
+    //        .Include(p => p.ProductImages)
+    //        .ToListAsync();
+    //}
+
+    //public override async Task<Product?> GetById(int id)
+    //{
+    //    return await _context.Products
+    //        .Include(p => p.Category)
+    //        .Include(P => P.Leasings)
+    //        .Include(p => p.ProductImages)
+    //        .SingleOrDefaultAsync(p => p.Id == id);
+    //}
 }
