6356795e8cb247f50778aab3c64f47196d6cf713
mohamedelofa <mohamedelofa7@gmail.com>
2024-10-31 21:56:39 +0300

    Edit AzureService

--- Croppilot.Services/Services/AzureBlobStorageService.cs
+++ Croppilot.Services/Services/AzureBlobStorageService.cs
@@ -2,21 +2,19 @@
 using Azure.Storage.Blobs;
 using Croppilot.Services.Abstract;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 
 namespace Croppilot.Services.Services
 {
 	internal class AzureBlobStorageService : IAzureBlobStorageService
 	{
-		private const string _key = "DefaultEndpointsProtocol=https;AccountName=elofatest2;AccountKey=WIM8ob5S83hHxxphqbu5BwzEEWH9ptPh3S/Ab3iNQp0/36UsmZB+KLH44ID+PHxoRh30YXFBlzMH+AStg27T9w==;EndpointSuffix=core.windows.net";
-		private const string _storageAccount = "elofatest2";
-		private const string _containerName = "elofatest26c7fc8ad-9001-40bb-8031-6443179f47e2";
 		private readonly BlobContainerClient _containerClient;
-        public AzureBlobStorageService()
+        public AzureBlobStorageService(IConfiguration configuration)
         {
 			_containerClient = new BlobServiceClient(
-				new Uri($"https://{_storageAccount}.blob.core.windows.net"),
-				new StorageSharedKeyCredential(_storageAccount, _key))
-				.GetBlobContainerClient(_containerName);
+				new Uri($"https://{configuration.GetSection("AzureKey:StorageAccount").Value}.blob.core.windows.net"),
+				new StorageSharedKeyCredential(configuration.GetSection("AzureKey:StorageAccount").Value, configuration.GetSection("AzureKey:ConnectionString").Value))
+				.GetBlobContainerClient(configuration.GetSection("AzureKey:ContainerName").Value);
 		}
         public async Task<bool> DeleteImageAsync(string filaName)
 		{
