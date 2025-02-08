30f84df1dbede0b070b5f78a3d9235f7e510c4ea
Mohamedzonol <mo.zonkol@gmail.com>
2025-02-08 18:39:59 +0200

    add controller

---
+++ Croppilot.API/Controller/LeasingController.cs
@@ -0,0 +1,59 @@
+﻿using Croppilot.API.Bases;
+using Croppilot.Core.Features.Leasing.Query.Models;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Croppilot.API.Controller
+{
+
+    public class LeasingController(IMediator mediator) : AppControllerBase
+    {
+        //[HttpPost("lease")]
+        //public async Task<IActionResult> LeaseProduct([FromBody] LeaseProductCommand command)
+        //{
+        //    var result = await mediator.Send(command);
+        //    return Ok(result);
+        //}
+
+        //[HttpPost("end/{id}")]
+        //public async Task<IActionResult> EndLease(int id, [FromBody] DateTime endDate)
+        //{
+        //    var result = await mediator.Send(new EndLeaseCommand(id, endDate));
+        //    return result == null ? NotFound() : Ok(result);
+        //}
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetLeasingById(int id)
+        {
+            var response = await mediator.Send(new GetLeasingByIdQuery(id));
+            return NewResult(response);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllLeasing()
+        {
+            var result = await mediator.Send(new GetAllLeasingsQuery());
+            return NewResult(result);
+        }
+        [HttpGet("{productId}")]
+        public async Task<IActionResult> GetLeasingByProductId(int productId)
+        {
+            var result = await mediator.Send(new GetLeasingsByProductIdQuery(productId));
+            return NewResult(result);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetActiveLeases(int userId)
+        {
+            var result = await mediator.Send(new GetActiveLeasesQuery());
+            return NewResult(result);
+        }
+
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> DeleteLeasing(int id)
+        //{
+        //    var result = await mediator.Send(new DeleteLeaseCommand(id));
+        //    return result ? Ok() : NotFound();
+        //}
+    }
+}
