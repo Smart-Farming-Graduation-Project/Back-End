9831820edf885ce0ee43ee27f22ffc08971c42ef
Mohamedzonol <mo.zonkol@gmail.com>
2025-02-10 19:41:12 +0200

    complete lockout Function and add it to login end point

--- Croppilot.Infrastructure/Comman/SD.cs
+++ Croppilot.Infrastructure/Comman/SD.cs
@@ -1,4 +1,4 @@
-﻿namespace Croppilot.Core.Comman
+﻿namespace Croppilot.Infrastructure.Comman
 {
     public static class SD
     {
@@ -7,5 +7,8 @@
         public const string ManagerRole = "Manager";
         public const string UserRole = "User";
 
+
+        public const int MaximumLoginAttempts = 5;
+        public const string AdminUserName = "admin@example.com";
     }
 }
--- Croppilot.Services/Services/AuthenticationService.cs
+++ Croppilot.Services/Services/AuthenticationService.cs
@@ -1,6 +1,7 @@
 ﻿using Croppilot.Date.Enum;
 using Croppilot.Date.Helpers;
 using Croppilot.Date.Identity;
+using Croppilot.Infrastructure.Comman;
 using Croppilot.Infrastructure.Repositories.Interfaces;
 using Croppilot.Services.Abstract;
 using Microsoft.AspNetCore.Identity;
@@ -12,162 +13,190 @@ using System.Text;
 
 namespace Croppilot.Services.Services
 {
-	internal class AuthenticationService : IAuthenticationService
-	{
-		private readonly UserManager<ApplicationUser> _userManager;
-		private readonly JwtSettings _jwtSettings;
-		private readonly IUnitOfWork _unitOfWork;
-		public AuthenticationService(UserManager<ApplicationUser> userManager, JwtSettings jwtSettings, IUnitOfWork unitOfWork)
-		{
-			_userManager = userManager;
-			_jwtSettings = jwtSettings;
-			_unitOfWork = unitOfWork;
-		}
+    internal class AuthenticationService : IAuthenticationService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly JwtSettings _jwtSettings;
+        private readonly IUnitOfWork _unitOfWork;
+        public AuthenticationService(UserManager<ApplicationUser> userManager, JwtSettings jwtSettings, IUnitOfWork unitOfWork)
+        {
+            _userManager = userManager;
+            _jwtSettings = jwtSettings;
+            _unitOfWork = unitOfWork;
+        }
 
-		public async Task<IdentityResult> ChangePasswordAsync(ApplicationUser user, string currentPassword, string newPassword)
-		{
-			return await _userManager.ChangePasswordAsync(user, currentPassword, newPassword);
-		}
+        public async Task<IdentityResult> ChangePasswordAsync(ApplicationUser user, string currentPassword, string newPassword)
+        {
+            return await _userManager.ChangePasswordAsync(user, currentPassword, newPassword);
+        }
 
-		public async Task<bool> CheckPasswordAsync(ApplicationUser user, string password)
-		{
-			return await _userManager.CheckPasswordAsync(user, password);
-		}
+        public async Task<bool> CheckPasswordAsync(ApplicationUser user, string password)
+        {
+            return await _userManager.CheckPasswordAsync(user, password);
+        }
 
-		public async Task<IdentityResult> CreateUserAsync(ApplicationUser user, string password)
-		{
-			var result = await _userManager.CreateAsync(user, password);
-			if (!result.Succeeded) return result;
-			return await _userManager.AddToRoleAsync(user, UserRoleEnum.User.ToString());
-		}
-
-
-		public async Task<TokenResponse> GetJWTtoken(ApplicationUser user)
-		{
-			var accessToken = await CreateTokenAsync(user);
-			var refreshToken = await GenerateRefreshTokenAsync(user);
-			return await Task.FromResult(new TokenResponse()
-			{
-				AccessToken = accessToken,
-				RefreshToken = refreshToken.Token,
-				RefreshTokenExpiration = refreshToken.ExpiresOn
-			});
-
-		}
-
-		private async Task<string> CreateTokenAsync(ApplicationUser user)
-		{
-			var jwtToken = new JwtSecurityToken(
-				issuer: _jwtSettings.Issuer,
-				expires: DateTime.Now.AddMinutes(_jwtSettings.AccessTokenDurationInMinutes),
-				signingCredentials: new SigningCredentials(
-					new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_jwtSettings.Key)),
-					SecurityAlgorithms.HmacSha256Signature
-					),
-				claims: await GetClaimsAsync(user)
-				);
-			var accessToken = new JwtSecurityTokenHandler().WriteToken(jwtToken);
-			return await Task.FromResult(accessToken);
-		}
-
-		private async Task<List<Claim>> GetClaimsAsync(ApplicationUser user)
-		{
-			var claims = new List<Claim>
-				{
-					new Claim(nameof(ClaimTypes.NameIdentifier),user.UserName ?? string.Empty),
-					new Claim(nameof(ClaimTypes.Name),$"{user.FirstName} {user.LastName}"),
-					new Claim(nameof(ClaimTypes.Email),user.Email ?? string.Empty)
-				};
-			var roles = await _userManager.GetRolesAsync(user);
-			foreach (var role in roles)
-			{
-				claims.Add(new Claim(nameof(ClaimTypes.Role), role));
-			}
-			return await Task.FromResult(claims);
-		}
+        public async Task<IdentityResult> CreateUserAsync(ApplicationUser user, string password)
+        {
+            var result = await _userManager.CreateAsync(user, password);
+            if (!result.Succeeded) return result;
+            return await _userManager.AddToRoleAsync(user, UserRoleEnum.User.ToString());
+        }
 
 
-		public async Task<TokenResponse> RefreshTokenAsync(ApplicationUser user, string refreshToken)
-		{
-			var response = new TokenResponse();
-			if (!await IsvalidRefreshToken(user, refreshToken))
-			{
-				response.RefreshToken = refreshToken;
-				return await Task.FromResult(response);
-			}
-			var userRefreshToken = user.RefreshTokens.First(r => r.Token == refreshToken);
-			userRefreshToken.RevokedOn = DateTime.UtcNow.ToLocalTime();
-			var newRefreshToken = await GenerateRefreshTokenAsync(user);
-			response.AccessToken = await CreateTokenAsync(user);
-			response.RefreshToken = newRefreshToken.Token;
-			response.RefreshTokenExpiration = newRefreshToken.ExpiresOn;
-			return await Task.FromResult(response);
-		}
+        public async Task<TokenResponse> GetJWTtoken(ApplicationUser user)
+        {
+            var accessToken = await CreateTokenAsync(user);
+            var refreshToken = await GenerateRefreshTokenAsync(user);
+            return await Task.FromResult(new TokenResponse()
+            {
+                AccessToken = accessToken,
+                RefreshToken = refreshToken.Token,
+                RefreshTokenExpiration = refreshToken.ExpiresOn
+            });
 
-		private async Task<bool> IsvalidRefreshToken(ApplicationUser user, string refreshToken)
-		{
-			if (user.RefreshTokens is not null && user.RefreshTokens.Any(r => r.Token == refreshToken))
-			{
-				var userRefreshToken = user.RefreshTokens.First(r => r.Token == refreshToken);
-				if (userRefreshToken.IsActive)
-				{
-					return await Task.FromResult(true);
-				}
-			}
-			return await Task.FromResult(false);
-		}
-		private async Task<RefreshToken> GenerateRefreshTokenAsync(ApplicationUser user)
-		{
-			//user.RefreshTokens = await GetRefreshTokensBelongToUserAsync(user.Id);
+        }
 
-			if (user?.RefreshTokens?.Any(r => r.IsActive) == true)
-			{
-				return await Task.FromResult(user.RefreshTokens.FirstOrDefault(r => r.IsActive));
-			}
-			var refreshToken = new RefreshToken()
-			{
-				Token = await CreateRefreshTokenAsync(),
-				ExpiresOn = DateTime.UtcNow.ToLocalTime().AddDays(_jwtSettings.RefreshTokenDurationInDays),
-				CreatedOn = DateTime.UtcNow.ToLocalTime(),
-				UserId = user.Id
-			};
-			refreshToken = await _unitOfWork.RefreshTokenRepository.AddAsync(refreshToken);
-			return await Task.FromResult(refreshToken);
-		}
+        private async Task<string> CreateTokenAsync(ApplicationUser user)
+        {
+            var jwtToken = new JwtSecurityToken(
+                issuer: _jwtSettings.Issuer,
+                expires: DateTime.Now.AddMinutes(_jwtSettings.AccessTokenDurationInMinutes),
+                signingCredentials: new SigningCredentials(
+                    new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_jwtSettings.Key)),
+                    SecurityAlgorithms.HmacSha256Signature
+                    ),
+                claims: await GetClaimsAsync(user)
+                );
+            var accessToken = new JwtSecurityTokenHandler().WriteToken(jwtToken);
+            return await Task.FromResult(accessToken);
+        }
+
+        private async Task<List<Claim>> GetClaimsAsync(ApplicationUser user)
+        {
+            var claims = new List<Claim>
+                {
+                    new Claim(nameof(ClaimTypes.NameIdentifier),user.UserName ?? string.Empty),
+                    new Claim(nameof(ClaimTypes.Name),$"{user.FirstName} {user.LastName}"),
+                    new Claim(nameof(ClaimTypes.Email),user.Email ?? string.Empty)
+                };
+            var roles = await _userManager.GetRolesAsync(user);
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(nameof(ClaimTypes.Role), role));
+            }
+            return await Task.FromResult(claims);
+        }
 
 
-		private async Task<string> CreateRefreshTokenAsync()
-		{
-			var randomNumber = new byte[64];
-			using (var rng = RandomNumberGenerator.Create())
-			{
-				rng.GetBytes(randomNumber);
-				return await Task.FromResult(Convert.ToBase64String(randomNumber));
-			}
+        public async Task<TokenResponse> RefreshTokenAsync(ApplicationUser user, string refreshToken)
+        {
+            var response = new TokenResponse();
+            if (!await IsvalidRefreshToken(user, refreshToken))
+            {
+                response.RefreshToken = refreshToken;
+                return await Task.FromResult(response);
+            }
+            var userRefreshToken = user.RefreshTokens.First(r => r.Token == refreshToken);
+            userRefreshToken.RevokedOn = DateTime.UtcNow.ToLocalTime();
+            var newRefreshToken = await GenerateRefreshTokenAsync(user);
+            response.AccessToken = await CreateTokenAsync(user);
+            response.RefreshToken = newRefreshToken.Token;
+            response.RefreshTokenExpiration = newRefreshToken.ExpiresOn;
+            return await Task.FromResult(response);
+        }
 
-		}
+        private async Task<bool> IsvalidRefreshToken(ApplicationUser user, string refreshToken)
+        {
+            if (user.RefreshTokens is not null && user.RefreshTokens.Any(r => r.Token == refreshToken))
+            {
+                var userRefreshToken = user.RefreshTokens.First(r => r.Token == refreshToken);
+                if (userRefreshToken.IsActive)
+                {
+                    return await Task.FromResult(true);
+                }
+            }
+            return await Task.FromResult(false);
+        }
+        private async Task<RefreshToken> GenerateRefreshTokenAsync(ApplicationUser user)
+        {
+            //user.RefreshTokens = await GetRefreshTokensBelongToUserAsync(user.Id);
 
-		public async Task<bool> RevokeRefreshTokenAsync(string refreshToken)
-		{
-			var token = await _unitOfWork.RefreshTokenRepository.GetAsync(r => r.Token.Equals(refreshToken));
-			if (token is null || !token.IsActive) return await Task.FromResult(false);
-			token.RevokedOn = DateTime.UtcNow.ToLocalTime();
-			await _unitOfWork.RefreshTokenRepository.UpdateAsync(token);
-			return await Task.FromResult(true);
-		}
+            if (user?.RefreshTokens?.Any(r => r.IsActive) == true)
+            {
+                return await Task.FromResult(user.RefreshTokens.FirstOrDefault(r => r.IsActive));
+            }
+            var refreshToken = new RefreshToken()
+            {
+                Token = await CreateRefreshTokenAsync(),
+                ExpiresOn = DateTime.UtcNow.ToLocalTime().AddDays(_jwtSettings.RefreshTokenDurationInDays),
+                CreatedOn = DateTime.UtcNow.ToLocalTime(),
+                UserId = user.Id
+            };
+            refreshToken = await _unitOfWork.RefreshTokenRepository.AddAsync(refreshToken);
+            return await Task.FromResult(refreshToken);
+        }
 
 
+        private async Task<string> CreateRefreshTokenAsync()
+        {
+            var randomNumber = new byte[64];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(randomNumber);
+                return await Task.FromResult(Convert.ToBase64String(randomNumber));
+            }
 
-		//private async Task<List<RefreshToken>> GetRefreshTokensBelongToUserAsync(string userId)
-		//{
-		//	return await _unitOfWork.RefreshTokenRepository.GetAllAsync(r => r.UserId.Equals(userId));
-		//}
+        }
+
+        public async Task<bool> RevokeRefreshTokenAsync(string refreshToken)
+        {
+            var token = await _unitOfWork.RefreshTokenRepository.GetAsync(r => r.Token.Equals(refreshToken));
+            if (token is null || !token.IsActive) return await Task.FromResult(false);
+            token.RevokedOn = DateTime.UtcNow.ToLocalTime();
+            await _unitOfWork.RefreshTokenRepository.UpdateAsync(token);
+            return await Task.FromResult(true);
+        }
+
+        public async Task<string?> CheckAndHandleLockoutAsync(ApplicationUser user)
+        {
+            if (user.LockoutEnd.HasValue && user.LockoutEnd.Value > DateTime.UtcNow)
+            {
+                return $"Your account has been locked until {user.LockoutEnd.Value:yyyy-MM-dd HH:mm:ss} UTC.";
+            }
+            return null;
+        }
+
+        public async Task HandleFailedLoginAsync(ApplicationUser user)
+        {
+            if (!user.UserName.Equals(SD.AdminUserName))
+            {
+                await _userManager.AccessFailedAsync(user);
+            }
+
+            if (user.AccessFailedCount >= SD.MaximumLoginAttempts)
+            {
+                var lockoutEnd = DateTime.UtcNow.AddDays(1);
+                await _userManager.SetLockoutEndDateAsync(user, lockoutEnd);
+            }
+        }
+
+        public async Task ResetFailedAttemptsAsync(ApplicationUser user)
+        {
+            await _userManager.ResetAccessFailedCountAsync(user);
+            await _userManager.SetLockoutEndDateAsync(user, null);
+        }
 
 
-		public async Task<List<RefreshToken>> GetRefreshTokens()
-		{
-			return await _unitOfWork.RefreshTokenRepository.GetAllAsync();
-		}
+        //private async Task<List<RefreshToken>> GetRefreshTokensBelongToUserAsync(string userId)
+        //{
+        //	return await _unitOfWork.RefreshTokenRepository.GetAllAsync(r => r.UserId.Equals(userId));
+        //}
 
-	}
+
+        public async Task<List<RefreshToken>> GetRefreshTokens()
+        {
+            return await _unitOfWork.RefreshTokenRepository.GetAllAsync();
+        }
+
+    }
 }
