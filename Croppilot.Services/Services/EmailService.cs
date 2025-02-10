27f45b432109cf148ff31e695c3fae887c0a6470
Mohamedzonol <mo.zonkol@gmail.com>
2025-02-10 11:57:21 +0200

    add confirm and forget password email

--- Croppilot.Services/Services/EmailService.cs
+++ Croppilot.Services/Services/EmailService.cs
@@ -1,12 +1,16 @@
 ﻿using Croppilot.Date.DTOS;
+using Croppilot.Date.Identity;
 using Croppilot.Services.Abstract;
 using Mailjet.Client;
 using Mailjet.Client.TransactionalEmails;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Configuration;
+using System.Text;
 
 namespace Croppilot.Services.Services
 {
-    public class EmailService(IConfiguration configuration) : IEmailService
+    public class EmailService(IConfiguration configuration, UserManager<ApplicationUser> userManager) : IEmailService
     {
         public async Task<bool> SendEmailAsync(EmailSendDto emailSend)
         {
@@ -29,6 +33,42 @@ namespace Croppilot.Services.Services
             }
 
             return false;
+        }
+
+
+        public async Task<bool> SendConfirmEMailAsync(ApplicationUser user)
+        {
+            var token = await userManager.GenerateEmailConfirmationTokenAsync(user);
+            token = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(token));
+            var url = $"{configuration["Jwt:ClientUrl"]}/{configuration["Email:ConfirmEmailPath"]}?token={token}&email={user.Email}";
+
+            var body = $"<p>Hello: {user.FirstName} {user.LastName}</p>" +
+                       "<p>Please confirm your email address by clicking on the following link.</p>" +
+                       $"<p><a href=\"{url}\">Click here</a></p>" +
+                       "<p>Thank you,</p>" +
+                       $"<br>{configuration["Email:ApplicationName"]}";
+
+            var emailSend = new EmailSendDto(user.Email, "Confirm your email", body);
+
+            return await SendEmailAsync(emailSend);
+        }
+
+        public async Task<bool> SendForgotUsernameOrPasswordEmail(ApplicationUser user)
+        {
+            var token = await userManager.GeneratePasswordResetTokenAsync(user);
+            token = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(token));
+            var url = $"{configuration["Jwt:ClientUrl"]}/{configuration["Email:ResetPasswordPath"]}?token={token}&email={user.Email}";
+
+            var body = $"<p>Hello: {user.FirstName} {user.LastName}</p>" +
+                       $"<p>Username: {user.UserName}.</p>" +
+                       "<p>In order to reset your password, please click on the following link.</p>" +
+                       $"<p><a href=\"{url}\">Click here</a></p>" +
+                       "<p>Thank you,</p>" +
+                       $"<br>{configuration["Email:ApplicationName"]}";
+
+            var emailSend = new EmailSendDto(user.Email, "Forgot username or password", body);
+
+            return await SendEmailAsync(emailSend);
         }
     }
 }
