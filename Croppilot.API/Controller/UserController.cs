fbf11c6734dc58b1505993b6e89409c1d628afcd
Mohamedzonol <mo.zonkol@gmail.com>
2025-02-13 19:14:51 +0200

    test ci/cd actions

--- Croppilot.API/Controller/UserController.cs
+++ Croppilot.API/Controller/UserController.cs
@@ -8,73 +8,74 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Croppilot.API.Controller
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	[Authorize(Policy = nameof(UserRoleEnum.Admin))]
-	public class UserController : AppControllerBase
-	{
-		private readonly IMediator _mediator;
-		public UserController(IMediator mediator)
-		{
-			_mediator = mediator;
-		}
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Policy = nameof(UserRoleEnum.Admin))]
+    public class UserController : AppControllerBase
+    {
+        private readonly IMediator _mediator;
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+
+        }
 
 
-		[HttpGet("GetUsers")]
-		public async Task<IActionResult> GetPaginatedUsers([FromQuery] int pageNumber, int pageSize)
-		{
-			return Ok(await _mediator.Send(new GetUserPaginatedQuery(pageNumber, pageSize)));
-		}
+        [HttpGet("GetUsers")]
+        public async Task<IActionResult> GetPaginatedUsers([FromQuery] int pageNumber, int pageSize)
+        {
+            return Ok(await _mediator.Send(new GetUserPaginatedQuery(pageNumber, pageSize)));
+        }
 
-		[HttpGet("GetById/{id:guid}")]
-		public async Task<IActionResult> GetById(string id)
-		{
-			var response = await _mediator.Send(new GetUserByIdQuery(id));
-			return NewResult(response);
-		}
+        [HttpGet("GetById/{id:guid}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var response = await _mediator.Send(new GetUserByIdQuery(id));
+            return NewResult(response);
+        }
 
-		[HttpGet("GetByName/{userName:alpha}")]
-		public async Task<IActionResult> GetByName(string userName)
-		{
-			var response = await _mediator.Send(new GetUserByUserNameQuery(userName));
-			return NewResult(response);
-		}
+        [HttpGet("GetByName/{userName:alpha}")]
+        public async Task<IActionResult> GetByName(string userName)
+        {
+            var response = await _mediator.Send(new GetUserByUserNameQuery(userName));
+            return NewResult(response);
+        }
 
-		[HttpGet("user-roles/Get/{userName:alpha}")]
-		public async Task<IActionResult> GetUserRoles(string userName)
-		{
-			return NewResult(await _mediator.Send(new GetUserRolesQuery() { UserName = userName }));
-		}
+        [HttpGet("user-roles/Get/{userName:alpha}")]
+        public async Task<IActionResult> GetUserRoles(string userName)
+        {
+            return NewResult(await _mediator.Send(new GetUserRolesQuery() { UserName = userName }));
+        }
 
-		[HttpPut("user-role/change")]
-		[Authorize(Policy = nameof(UserRoleEnum.SuperAdmin))]
-		public async Task<IActionResult> ChangeUserRole(ChangeUserRoleCommand command)
-		{
-			return NewResult(await _mediator.Send(command));
-		}
+        [HttpPut("user-role/change")]
+        [Authorize(Policy = nameof(UserRoleEnum.SuperAdmin))]
+        public async Task<IActionResult> ChangeUserRole(ChangeUserRoleCommand command)
+        {
+            return NewResult(await _mediator.Send(command));
+        }
 
-		[HttpGet("user-role/Get/{roleName:alpha}")]
-		public async Task<IActionResult> GetUsersAssignedToRole(string roleName)
-		{
-			return NewResult(await _mediator.Send(new GetUsersAssignedToRoleQuery() { RoleName = roleName }));
-		}
+        [HttpGet("user-role/Get/{roleName:alpha}")]
+        public async Task<IActionResult> GetUsersAssignedToRole(string roleName)
+        {
+            return NewResult(await _mediator.Send(new GetUsersAssignedToRoleQuery() { RoleName = roleName }));
+        }
 
-		[HttpPut("Edit")]
-		public async Task<IActionResult> EditUser(EditUserCommand command)
-		{
-			return NewResult(await _mediator.Send(command));
-		}
+        [HttpPut("Edit")]
+        public async Task<IActionResult> EditUser(EditUserCommand command)
+        {
+            return NewResult(await _mediator.Send(command));
+        }
 
-		[HttpDelete("Delete/{id:guid}")]
-		public async Task<IActionResult> Delete(string id)
-		{
-			return NewResult(await _mediator.Send(new DeleteUserCommand(id)));
-		}
-		[HttpDelete("user-role/remove")]
-		[Authorize(Policy = nameof(UserRoleEnum.SuperAdmin))]
-		public async Task<IActionResult> RemoveUserRole(RemoveUserFromRoleCommand command)
-		{
-			return NewResult(await _mediator.Send(command));
-		}
-	}
+        [HttpDelete("Delete/{id:guid}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            return NewResult(await _mediator.Send(new DeleteUserCommand(id)));
+        }
+        [HttpDelete("user-role/remove")]
+        [Authorize(Policy = nameof(UserRoleEnum.SuperAdmin))]
+        public async Task<IActionResult> RemoveUserRole(RemoveUserFromRoleCommand command)
+        {
+            return NewResult(await _mediator.Send(command));
+        }
+    }
 }
