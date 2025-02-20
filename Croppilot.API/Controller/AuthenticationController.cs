5882370edb45a9f3cbcebd7723a86d61e9c1c176
Mohamedzonol <mo.zonkol@gmail.com>
2025-02-20 16:24:46 +0200

    add some docs

--- Croppilot.API/Controller/AuthenticationController.cs
+++ Croppilot.API/Controller/AuthenticationController.cs
@@ -20,8 +20,6 @@ namespace Croppilot.API.Controller;
 /// </summary>
 [Route("api/[controller]")]
 [ApiController]
-[SwaggerResponse(200, "Operation is done successfully")]
-[SwaggerResponse(400, "Invalid operation or something is invalid")]
 public class AuthenticationController(IMediator mediator) : AppControllerBase
 {
     /// <summary>
@@ -30,6 +28,8 @@ public class AuthenticationController(IMediator mediator) : AppControllerBase
     /// If errors occur (e.g., duplicate email), the response 'Errors' list will contain the problematic field (e.g., "Email").
     /// </summary>
     [HttpPost("Register")]
+    [SwaggerResponse(201, "Your account has been created, please confirm your email address")]
+    [SwaggerResponse(400, "Bad Request. Possible reasons 'Messages returned': \n- User creation failed \n- Failed to send email. Please contact admin \n- some error in operation it will appear in message 'دي مش الرساله اللي هتظهر انا بوضحكلك'")]
     [SwaggerOperation(
         Summary = "Registers a new user",
         Description = "**Creates a new user account with the provided details.**")]
@@ -44,6 +44,7 @@ public class AuthenticationController(IMediator mediator) : AppControllerBase
     /// On failure (e.g., invalid credentials or unconfirmed email), check the 'Errors' for details.
     /// </summary>
     [HttpPost("SignIn")]
+    [SwaggerResponse(400, "Bad Request. Possible reasons 'Messages returned': \n- Invalid username or password. \n- Please confirm your email before signing in. \n- Username or Password are wrong \n- others")]
     [SwaggerOperation(
         Summary = "Log in a user",
         Description = "**Authenticates a user and returns an access token.**")]
@@ -86,6 +87,8 @@ public class AuthenticationController(IMediator mediator) : AppControllerBase
     /// On failure, errors may include issues with the "Email" or "Token" fields.
     /// </summary>
     [HttpPut("confirm-email")]
+    [SwaggerResponse(200, "Your email address is confirmed. You can login now")]
+    [SwaggerResponse(400, "Bad Request. Possible reasons 'Messages returned': \n- This email address has not been registered yet \n- Invalid token. Please try again \n- Your email was confirmed before. Please login to your account")]
     [SwaggerOperation(
         Summary = "Confirms a user's email",
         Description = "**Verifies the user's email using a confirmation token.**")]
@@ -128,6 +131,9 @@ public class AuthenticationController(IMediator mediator) : AppControllerBase
     /// On failure, errors may include issues with the "Token" or "Email" fields.
     /// </summary>
     [HttpPut("reset-password")]
+    [SwaggerResponse(200, "Please check your email")]
+    [SwaggerResponse(400, "Bad Request. Possible reasons 'Messages returned': \n- This email address has not been registered yet \n- Please confirm your email address first. \n- Failed to send email. Please contact admin")]
+
     [SwaggerOperation(
         Summary = "Resets the user password",
         Description = "**Allows a user to reset their password using a token.**")]
