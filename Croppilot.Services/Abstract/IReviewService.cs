537ad828534439d58a710d7814b126ab0f6ee225
Reda Elsayed <redaelsayied.work@gmail.com>
2025-02-21 17:31:30 +0200

    add ReviewService

---
+++ Croppilot.Services/Abstract/IReviewService.cs
@@ -0,0 +1,8 @@
+﻿namespace Croppilot.Services.Abstract;
+
+public interface IReviewService
+{
+    Task<OperationResult> AddReviewAsync(Review review, CancellationToken cancellationToken = default);
+    Task<List<Review>> GetReviewsByProductIdAsync(int productId, CancellationToken cancellationToken = default);
+    Task<OperationResult> DeleteReviewAsync(int reviewId, CancellationToken cancellationToken = default);
+}
---
+++ Croppilot.Services/Services/ReviewService.cs
@@ -0,0 +1,29 @@
+﻿using Croppilot.Infrastructure.Repositories.Interfaces;
+
+namespace Croppilot.Services.Services;
+
+public class ReviewService(IReviewRepository reviewRepository) : IReviewService
+{
+    public async Task<OperationResult> AddReviewAsync(Review review, CancellationToken cancellationToken = default)
+    {
+        await reviewRepository.AddAsync(review, cancellationToken);
+        return OperationResult.Success;
+    }
+
+    public async Task<List<Review>> GetReviewsByProductIdAsync(int productId,
+        CancellationToken cancellationToken = default)
+    {
+        return await reviewRepository.GetReviewsByProductIdAsync(productId, cancellationToken);
+    }
+
+    public async Task<OperationResult> DeleteReviewAsync(int reviewId, CancellationToken cancellationToken = default)
+    {
+        var review =
+            await reviewRepository.GetAsync(r => r.ReviewID == reviewId, cancellationToken: cancellationToken);
+        if (review == null)
+            return OperationResult.Failure;
+
+        await reviewRepository.DeleteAsync(review, cancellationToken);
+        return OperationResult.Success;
+    }
+}
