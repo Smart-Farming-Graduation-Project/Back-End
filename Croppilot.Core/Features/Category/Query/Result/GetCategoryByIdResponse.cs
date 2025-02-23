f93f2cf5690a933e7f4addc31cb9e0b611759a84
Reda Elsayed <redaelsayied.work@gmail.com>
2025-02-24 00:32:10 +0200

    fix mapping between GetCategoryByIdQuery and Category to return data frontend needd

--- Croppilot.Core/Features/Category/Query/Result/GetCategoryByIdResponse.cs
+++ Croppilot.Core/Features/Category/Query/Result/GetCategoryByIdResponse.cs
@@ -16,7 +16,9 @@ namespace Croppilot.Core.Features.Category.Query.Result
     {
         public int ProductId { get; set; }
         public string ProductName { get; set; }
+        public string Description { get; set; }
         public string Availability { get; set; }
         public decimal Price { get; set; }
+        public List<string> Images { get; set; } = new();
     }
 }
