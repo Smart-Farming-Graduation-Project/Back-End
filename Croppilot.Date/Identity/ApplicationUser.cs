9ff81353b5f5e05ec814060d78309311adb567a2
Reda Elsayed <redaelsayied.work@gmail.com>
2025-02-27 14:19:15 +0200

    fix allow null .. you make validation notallow null and here you allow null!!

--- Croppilot.Date/Identity/ApplicationUser.cs
+++ Croppilot.Date/Identity/ApplicationUser.cs
@@ -5,8 +5,8 @@ namespace Croppilot.Date.Identity
 {
     public class ApplicationUser : IdentityUser
     {
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string? Address { get; set; }
         public string? Phone { get; set; } = null!;
         public string? OTPCode { get; set; } = null;
