05156a2f8f364911220321b63f7a3465cab29f26
Reda Elsayed <redaelsayied.work@gmail.com>
2025-03-09 01:23:47 +0200

    add post service implementation : Create IPostService and PostService to manage post operations

---
+++ Croppilot.Services/Abstract/IPostService.cs
@@ -0,0 +1,10 @@
+﻿namespace Croppilot.Services.Abstract;
+
+public interface IPostService
+{
+    Task<OperationResult> AddPostAsync(Post post, CancellationToken cancellationToken = default);
+    Task<List<Post>> GetAllPostsAsync(CancellationToken cancellationToken = default);
+    Task<Post?> GetPostByIdAsync(int id, CancellationToken cancellationToken = default);
+    Task<OperationResult> DeletePostAsync(int id, CancellationToken cancellationToken = default);
+    Task<OperationResult> UpdatePostAsync(Post post, CancellationToken cancellationToken = default);
+}
