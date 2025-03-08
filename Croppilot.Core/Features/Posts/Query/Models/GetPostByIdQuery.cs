ce70949ca3dfa46c1da29f5bc48f53e8d3ed5f58
Reda Elsayed <redaelsayied.work@gmail.com>
2025-03-09 01:33:07 +0200

    add post query implementation : Introduce GetPostByIdQuery and GetPostsQuery along with PostQueryHandler to handle post retrieval operations

---
+++ Croppilot.Core/Features/Posts/Query/Models/GetPostByIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using Croppilot.Core.Features.Posts.Query.Result;
+
+namespace Croppilot.Core.Features.Posts.Query.Models;
+
+public class GetPostByIdQuery : IRequest<Response<PostResponse>>
+{
+    public int Id { get; set; }
+}
