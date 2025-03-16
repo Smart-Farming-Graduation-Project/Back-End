4e184673c9573086d98cee2d8c4c6598216b42aa
mohamedelofa <mohamedelofa7@gmail.com>
2025-03-16 02:33:02 +0200

    update create order request (get userid from claims) and create order response for payment operation

--- Croppilot.Core/Features/Orders/Command/Validators/CreateOrderCommandValidator.cs
+++ Croppilot.Core/Features/Orders/Command/Validators/CreateOrderCommandValidator.cs
@@ -4,16 +4,15 @@ namespace Croppilot.Core.Features.Orders.Command.Validators;
 
 public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
 {
-    public CreateOrderCommandValidator()
-    {
-        RuleFor(x => x.UserId).NotEmpty().WithMessage("UserId is required.");
+	public CreateOrderCommandValidator()
+	{
 
-        RuleFor(x => x.ShippingAddress)
-            .NotEmpty().WithMessage("Shipping Address is required.");
+		RuleFor(x => x.ShippingAddress)
+			.NotEmpty().WithMessage("Shipping Address is required.");
 
-        RuleFor(x => x.OrderItems)
-            .NotEmpty().WithMessage("At least one order item is required.");
+		RuleFor(x => x.OrderItems)
+			.NotEmpty().WithMessage("At least one order item is required.");
 
-        RuleForEach(x => x.OrderItems).SetValidator(new CreateOrderItemCommandValidator());
-    }
+		RuleForEach(x => x.OrderItems).SetValidator(new CreateOrderItemCommandValidator());
+	}
 }
