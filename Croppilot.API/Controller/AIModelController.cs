df16403d5c57998ad370399a339718b263333878
Reda Elsayed <redaelsayied.work@gmail.com>
2025-04-25 20:18:19 +0300

    add: enable rate limiting for AIModelController endpoints

--- Croppilot.API/Controller/AIModelController.cs
+++ Croppilot.API/Controller/AIModelController.cs
@@ -4,21 +4,23 @@ namespace Croppilot.API.Controller
 {
     [Route("api/[controller]")]
     [ApiController]
+    [EnableRateLimiting(RateLimiters.AIModelRateLimit)]
     public class AIModelController(IMediator mediator) : AppControllerBase
     {
-
         [HttpPost("predict")]
         public async Task<IActionResult> Predict(PredictModelCommand command)
         {
             var result = await mediator.Send(command);
             return NewResult(result);
         }
+
         [ResponseCache(CacheProfileName = "NoCache"), HttpGet("feedback/{ImageId}")]
         public async Task<IActionResult> Feedback(Guid ImageId)
         {
             var result = await mediator.Send(new GetFeedback(ImageId));
             return NewResult(result);
         }
+
         [ResponseCache(CacheProfileName = "NoCache"), HttpPost("wateringFeedback")]
         public async Task<IActionResult> WateringFeedback(GetWateringFeedback command)
         {
@@ -28,12 +30,12 @@ namespace Croppilot.API.Controller
             var result = await mediator.Send(command);
             return NewResult(result);
         }
+
         [ResponseCache(CacheProfileName = "Default"), HttpGet("GetCurrentValue")]
         public async Task<IActionResult> GetCurrentValue()
         {
             var result = await mediator.Send(new GetCurrentValue());
             return NewResult(result);
         }
-
     }
 }
