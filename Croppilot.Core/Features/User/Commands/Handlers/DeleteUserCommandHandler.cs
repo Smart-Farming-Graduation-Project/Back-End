ca7faee6dec68ef8fa38424179c796bb2cf374da
mohamedelofa <mohamedelofa7@gmail.com>
2025-04-27 19:26:54 +0300

    Enhance API documentation and resource management
    Updated Swagger summaries and descriptions in
    AuthenticationController, CategoryController, and UserController
    for improved clarity. Enhanced DeleteUserCommandHandler
    to manage user images in Azure Blob Storage upon deletion.
    Overall, these changes improve API usability and resource handling.

--- Croppilot.Core/Features/User/Commands/Handlers/DeleteUserCommandHandler.cs
+++ Croppilot.Core/Features/User/Commands/Handlers/DeleteUserCommandHandler.cs
@@ -1,18 +1,23 @@
 ﻿using Croppilot.Core.Features.User.Commands.Models;
-using Croppilot.Services.Abstract;
 
 namespace Croppilot.Core.Features.User.Commands.Handlers
 {
-	internal class DeleteUserCommandHandler(IUserService service)
+	internal class DeleteUserCommandHandler(IUserService service, IAzureBlobStorageService azureBlobStorageService)
 		: ResponseHandler, IRequestHandler<DeleteUserCommand, Response<string>>
 	{
 		public async Task<Response<string>> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
 		{
 			var user = await service.GetUserById(request.Id);
 			if (user is null) return NotFound<string>("User does not exist");
+			var ImageUrl = user.ImageUrl;
+			if (!string.IsNullOrEmpty(ImageUrl))
+			{
+				var blobName = ImageUrl?.Split('/').Last();
+				await azureBlobStorageService.DeleteImageAsync(blobName, "user-images");
+			}
 			var result = await service.DeleteUserAsync(user);
-			if (result.Succeeded) return Success(string.Empty);
-			return BadRequest<string>(result.Errors.FirstOrDefault().Description);
+			if (result.Succeeded) return Success("User Deleted");
+			return BadRequest<string>("Failed to delete user");
 		}
 	}
 }
