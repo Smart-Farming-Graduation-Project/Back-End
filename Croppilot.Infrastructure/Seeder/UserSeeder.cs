573ed3b39b4450d2b0c5e60b7b3ce0020e3be5f3
Mohamedzonol <mo.zonkol@gmail.com>
2025-04-29 15:57:58 +0300

    fix seeding problem

--- Croppilot.Infrastructure/Seeder/UserSeeder.cs
+++ Croppilot.Infrastructure/Seeder/UserSeeder.cs
@@ -4,25 +4,26 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Croppilot.Infrastructure.Seeder
 {
-	public static class UserSeeder
-	{
-		public static async Task SeedAsync(UserManager<ApplicationUser> userManager)
-		{
-			if (!(await userManager.Users.CountAsync() > 0))
-			{
-				var user = new ApplicationUser()
-				{
-					FirstName = "default",
-					LastName = "admin",
-					UserName = "admin",
-					Email = "admin@gmail.com",
-					Address = "Zifta",
-					Phone = "01234567890",
-					EmailConfirmed = true
-				};
-				await userManager.CreateAsync(user, "Mo123#");
-				await userManager.AddToRoleAsync(user, UserRoleEnum.Admin.ToString());
-			}
-		}
-	}
+    public static class UserSeeder
+    {
+        public static async Task SeedAsync(UserManager<ApplicationUser> userManager)
+        {
+            if (!(await userManager.Users.CountAsync() > 0))
+            {
+                var user = new ApplicationUser()
+                {
+                    FirstName = "default",
+                    LastName = "admin",
+                    UserName = "admin",
+                    Email = "admin@gmail.com",
+                    Address = "Zifta",
+                    Phone = "01234567890",
+                    ImageUrl = "https://example.com/image.jpg",
+                    EmailConfirmed = true
+                };
+                await userManager.CreateAsync(user, "Mo123#");
+                await userManager.AddToRoleAsync(user, UserRoleEnum.Admin.ToString());
+            }
+        }
+    }
 }
