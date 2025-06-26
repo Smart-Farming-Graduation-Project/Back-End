d85c56ae315c554a49f2e02e97aa1b7aca49be37
Reda Elsayed <redaelsayied.work@gmail.com>
2025-06-26 15:31:26 +0300

    fix: change Review rating type from int to double for improved precision

--- Croppilot.Core/Features/Reviews/Command/Models/AddReviewCommand.cs
+++ Croppilot.Core/Features/Reviews/Command/Models/AddReviewCommand.cs
@@ -4,7 +4,7 @@ public class AddReviewCommand : IRequest<Response<string>>
 {
     public int ProductID { get; set; }
     public string Headline { get; set; }
-    public int Rating { get; set; }
+    public double Rating { get; set; }
     public string ReviewText { get; set; }
 
 }
